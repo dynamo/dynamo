7e0ed1c8424195bdc2c03d2a5466aa663f334f86
José Valim <jose.valim@gmail.com>
2012-01-08 13:45:46 +0100

    Implement branch merging.

--- lib/router/gtg.ex
+++ lib/router/gtg.ex
@@ -6,6 +6,14 @@ defmodule Dynamo::Router::GTG do
     visit(ast, endpoint)
   end
 
+  # Merge a list of branches given as left and the
+  # branch given as right.
+  def merge(left, right) do
+    merge(left, right, [])
+  end
+
+  ## Helpers related to branch visiting
+
   defp visit({ :cat, left, right }, endpoint) do
     left  = terminal(left)
     right = visit(right, endpoint)
@@ -13,12 +21,30 @@ defmodule Dynamo::Router::GTG do
   end
 
   defp visit(tuple, endpoint) do
-    { terminal(tuple), [{ :endpoint, endpoint }] }
+    { terminal(tuple), [{ :endpoint, [endpoint] }] }
   end
-
-  # Handle terminals
 
   defp terminal({ :terminal, contents }) do
     contents
+  end
+
+  ## Helpers related to merging visiting
+
+  # If the two items are equal, jackpot! We should merge old
+  # with new and then rewind acc on top of the tail.
+  defp merge([{ left, old }|t], { left, new }, acc) do
+    merged = Enum.foldl new, old, fn(x, acc) { merge(acc, x, []) }
+    Enum.foldl [{left, merged}|acc], t, fn(x, acc) { [x|acc] }
+  end
+
+  # If not equal, keep searching.
+  defp merge([h|t], tuple, acc) do
+    merge(t, tuple, [h|acc])
+  end
+
+  # If we reached the end of the list, add the tuple at
+  # the end and reverse.
+  defp merge([], tuple, acc) do
+    List.reverse [tuple|acc]
   end
 end
--- test/router/gtg_test.exs
+++ test/router/gtg_test.exs
@@ -9,12 +9,52 @@ defmodule Dynamo::Router::GTGTest do
   def test_branch do
     { 'foo', [
       {'/', [
-        {'bar', [ { :endpoint, :END } ] }
+        {'bar', [ { :endpoint, [:END] } ] }
       ] }
     ] } = branch('foo/bar', :END)
   end
 
+  def test_branch_with_literal do
+    { 'foo', [ { :endpoint, [:END] } ] } = branch('foo', :END)
+  end
+
+  def test_merge_distinct do
+    [
+      { 'foo', [ {:endpoint, [:A] } ] },
+      { 'bar', [ {:endpoint, [:B] } ] }
+    ] = merge('foo', 'bar')
+  end
+
+  def test_merge_nested_similar do
+    [
+      { 'foo', [
+        { '/', [
+          { 'bar', [ {:endpoint, [:A] } ] },
+          { 'baz', [ {:endpoint, [:B] } ] }
+        ] }
+      ] }
+    ] = merge('foo/bar', 'foo/baz')
+  end
+
+  def test_merge_different_endpoints do
+    [
+      { 'foo', [
+        { '/', [
+          { 'bar', [
+            { :endpoint, [:A, :B] }
+          ] }
+        ] }
+      ] }
+    ] = merge('foo/bar', 'foo/bar')
+  end
+
   defp branch(string, endpoint) do
     GTG.branch Parser.parse(string), endpoint
+  end
+
+  defp merge(a, b) do
+    a = GTG.branch Parser.parse(a), :A
+    b = GTG.branch Parser.parse(b), :B
+    GTG.merge [a], b
   end
 end
