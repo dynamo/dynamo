89c1333f4eeb566d4c913cc3bb64adeae0127a65
José Valim <jose.valim@gmail.com>
2012-02-12 14:17:24 +0100

    Improve docs.

--- lib/dynamo/router/compiler.ex
+++ lib/dynamo/router/compiler.ex
@@ -1,6 +1,11 @@
 defmodule Dynamo::Router::Compiler do
-  # Receives merged branches as `gtg` and converts them to a
-  # series of compiled functions injected into the given module.
+  # Main entry point for compilation, called recursively
+  # during the compilation process. It works with two counters,
+  # one indicates the name of the function where the current route
+  # is being compiled and the other indicates the name to where the
+  # current route should point to.
+  #
+  # All the functions are compiled straight into the given module.
   def compile(module, gtg, counter // 0) do
     { contents, result } = Enum.map_reduce gtg, counter + 1, compile_each(&1, &2, module, counter)
     contents = contents ++ [error_for(name_for(counter))]
@@ -8,10 +13,9 @@ defmodule Dynamo::Router::Compiler do
     result
   end
 
-  # The endpoint is special cased because the second element
-  # of the tuple is not a list. If we have an endpoint, it
-  # means we just need to add the Final clause using the
-  # verb and the extra parameters given.
+  # The endpoint is a special node and needs to be special cased.
+  # In particular, the endpoint does not point to other nodes
+  # and should just return the final result.
   defp compile_each({ :endpoint, { verb, extra } }, counter, _module, current) do
     name = name_for(current)
 
@@ -25,6 +29,9 @@ defmodule Dynamo::Router::Compiler do
   end
 
   # Each other item besides the endpoint is simply compiled recursively.
+  # Those other items are first flattened (allowing us to remove hops
+  # in the matching algorithm) and then compiled according to the
+  # `contents_for` function.
   defp compile_each(item, counter, module, current) do
     { left, right } = flatten(item)
 
@@ -49,9 +56,10 @@ defmodule Dynamo::Router::Compiler do
     end
   end
 
-  # If the left part only points to another item
-  # and both of them are literals, we can concatenate
-  # them together to avoid matching.
+  # If the current node is a literal (like 'foo') and it points
+  # to another single literal node (like 'bar'), we can simply
+  # concatenate them now and avoid extra hops when doing the
+  # recognition.
   defp flatten({ left, [{ right, args }] }) when is_list(left) andalso is_list(right) do
     flatten({ left ++ right, args })
   end
@@ -60,7 +68,8 @@ defmodule Dynamo::Router::Compiler do
     tuple
   end
 
-  # Returns an error route. Used as last clause.
+  # Returns an error route. Used as last clause so we
+  # return an atom :error instead of raising an exception.
   defp error_for(name) do
     quote do
       defp unquote(name).(_, _, _), do: :error
