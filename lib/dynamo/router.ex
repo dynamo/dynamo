76f66d6fc58a272e6821fdf379d87be5ccb58afb
José Valim <jose.valim@gmail.com>
2012-02-12 20:27:51 +0100

    Implement splitting for the new router.

--- lib/dynamo/router.ex
+++ lib/dynamo/router.ex
@@ -1,2 +1,30 @@
 defmodule Dynamo::Router do
+  # Splits the given path into several segments.
+  # It ignores both leading and trailing slashes in the path.
+  #
+  # ## Examples
+  #
+  #     split("/foo/bar") #=> ['foo', 'bar']
+  #
+  def split([?/|t]) do
+    split t
+  end
+
+  def split(t) do
+    split t, [], []
+  end
+
+  ## Helpers
+
+  defp split(list, buffer, acc) when list == [] orelse list == [?/] do
+    List.reverse [List.reverse(buffer)|acc]
+  end
+
+  defp split([?/|t], buffer, acc) do
+    split t, [], [List.reverse(buffer)|acc]
+  end
+
+  defp split([h|t], buffer, acc) do
+    split t, [h|buffer], acc
+  end
 end
--- test/dynamo/router_test.exs
+++ test/dynamo/router_test.exs
@@ -1,5 +1,21 @@
 Code.require_file "../../test_helper", __FILE__
 
 defmodule Dynamo::RouterTest do
+  require Dynamo::Router, as: R
   use ExUnit::Case
+
+  def test_split_single_segment do
+    assert_equal ['foo'], R.split('/foo')
+    assert_equal ['foo'], R.split('foo')
+  end
+
+  def test_split_with_more_than_one_segment do
+    assert_equal ['foo', 'bar'], R.split('/foo/bar')
+    assert_equal ['foo', 'bar'], R.split('foo/bar')
+  end
+
+  def test_split_removes_trailing_slash do
+    assert_equal ['foo', 'bar'], R.split('/foo/bar/')
+    assert_equal ['foo', 'bar'], R.split('foo/bar/')
+  end
 end
