e797594fab936d840fba009f02e9f5558082ee6f
José Valim <jose.valim@plataformatec.com.br>
2012-08-28 11:06:58 -0300

    Add tests for dynamo compilation

--- test/mix/tasks/dynamo_test.exs
+++ test/mix/tasks/dynamo_test.exs
@@ -34,7 +34,7 @@ defmodule Mix.Tasks.DynamoTest do
   end
 
   test "generates a dynamo app for development" do
-    in_tmp "my_app", fn ->
+    in_tmp "my_dev_app", fn ->
       Mix.Tasks.Dynamo.run [".", "--dev"]
 
       assert_file "mix.exs", fn(file) ->
@@ -43,6 +43,19 @@ defmodule Mix.Tasks.DynamoTest do
 
       assert_received { :mix_shell, :info, ["* creating mix.exs"] }
       assert_received { :mix_shell, :info, ["* creating config/app.ex"] }
+    end
+  end
+
+  test "generates and compiles an application" do
+    in_tmp "my_compiled_app", fn ->
+      Mix.Tasks.Dynamo.run [".", "--dev"]
+
+      File.write! "mix.exs", Regex.replace(%r"deps: deps", File.read!("mix.exs"), %b(deps: deps, deps_path: "../../deps"))
+      output = System.cmd "MIXENV=prod mix compile"
+
+      assert output =~ %r(Compiled app/routers/application_router.ex)
+      assert output =~ %r(Compiled config/app.ex)
+      assert output =~ %r(Generated my_compiled_app.app)
     end
   end
 end
