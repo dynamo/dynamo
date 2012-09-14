e316f7bcbab3eae6e9475729ceb92ec287906e55
José Valim <jose.valim@plataformatec.com.br>
2012-09-14 12:18:44 +0200

    Break test deps apart

--- mix.exs
+++ mix.exs
@@ -4,14 +4,20 @@ defmodule Dynamo.Mixfile do
   def project do
     [ app: :dynamo,
       version: "0.1.0.dev",
-      deps: deps ]
+      deps: deps,
+      env:
+        [test: [deps: test_deps]] ]
   end
 
   def deps do
     [ { :mimetypes, git: "https://github.com/spawngrid/mimetypes.git" },
-      { :edown, git: "https://github.com/esl/edown.git" }, ## Hackney dependency
-      { :hackney, git: "https://github.com/benoitc/hackney.git" },
       { :cowboy,  git: "https://github.com/josevalim/cowboy.git" } ]
+  end
+
+  def test_deps do
+    deps ++
+      [ { :edown, git: "https://github.com/esl/edown.git" }, ## Hackney dependency
+        { :hackney, git: "https://github.com/benoitc/hackney.git" } ]
   end
 
   def application do
