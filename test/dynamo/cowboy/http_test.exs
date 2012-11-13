2ead7219fd6a349b566311acd31d931009aaec1f
José Valim <jose.valim@plataformatec.com.br>
2012-11-13 22:45:33 +0100

    Update to latest hackney

--- test/dynamo/cowboy/http_test.exs
+++ test/dynamo/cowboy/http_test.exs
@@ -313,9 +313,7 @@ defmodule Dynamo.Cowboy.HTTPTest do
   end
 
   def send_with_head(conn) do
-    IO.puts "There is a bug in hackney which makes it fails with send on head"
-    # conn.send(200, "OK")
-    conn.send(200, "")
+    conn.send(200, "OK")
   end
 
   test :send do
