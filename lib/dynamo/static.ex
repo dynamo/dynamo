0db33b9e2fbe18be8400a5c98b749dd9067848f3
José Valim <jose.valim@plataformatec.com.br>
2012-12-22 16:06:56 +0100

    Add support to cached static assets lookup

---
+++ lib/dynamo/static.ex
@@ -0,0 +1,86 @@
+defmodule Dynamo.Static do
+  @moduledoc false
+
+  use GenServer.Behaviour
+
+  @doc """
+  Looks up a static asset in the given application.
+  If the static asset exists, it returns the asset
+  path appended with a query string containing the
+  last modification time of the asset.
+  """
+  def lookup(app, path) do
+    path = normalize_path(path)
+    { ets, server } = app.static_cache
+    case :ets.lookup(ets, path) do
+      [{ ^path, result }] -> result
+      _ -> :gen_server.call(server, { :lookup, path })
+    end
+  end
+
+  defp normalize_path("/" <> bin), do: bin
+  defp normalize_path(bin),        do: bin
+
+  ## Backend
+
+  @doc false
+  def start_link(app) do
+    { _ets, server } = app.static_cache
+    :gen_server.start({ :local, server }, __MODULE__, app, [])
+  end
+
+  @doc false
+  def stop(app) do
+    { _ets, server } = app.static_cache
+    :gen_server.call(server, :stop)
+  end
+
+  defrecord Config, [:ets, :route, :root, :cache]
+
+  @doc false
+  def init(app) do
+    dynamo  = app.config[:dynamo]
+    otp_app = dynamo[:otp_app]
+    root    = File.expand_path(dynamo[:static_root], lib_dir(otp_app))
+    route   = dynamo[:static_route]
+    cache   = dynamo[:cache_static]
+
+    { ets, _server } = app.static_cache
+    :ets.new(ets, [:set, :protected, :named_table, { :read_concurrency, true }])
+    { :ok, Config[ets: ets, route: route, root: root, cache: cache] }
+  end
+
+  @doc false
+  def handle_call({ :lookup, path }, _from, Config[] = config) do
+    result = static_path(path, config)
+    if config.cache, do: :ets.insert(config.ets, { path, result })
+    { :reply, result, config }
+  end
+
+  def handle_call(:stop, _from, config) do
+    { :stop, :normal, :ok, config }
+  end
+
+  def handle_call(_msg, _from, _config) do
+    super
+  end
+
+  ## Server helpers
+
+  defp lib_dir(app) do
+    case :code.lib_dir(app) do
+      list when is_list(list) -> list
+      _ -> File.cwd!
+    end
+  end
+
+  defp static_path(path, Config[] = config) do
+    case File.stat(File.join(config.root, path)) do
+      { :ok, File.Stat[mtime: mtime, type: type] } when type != :directory and is_tuple(mtime) ->
+        seconds = :calendar.datetime_to_gregorian_seconds(mtime)
+        File.join(config.route, [path, ??, integer_to_list(seconds)])
+      _ ->
+        File.join(config.route, path)
+    end
+  end
+end
---
+++ test/dynamo/static_test.exs
@@ -0,0 +1,71 @@
+Code.require_file "../../test_helper.exs", __FILE__
+
+defmodule Dynamo.StaticTest do
+  use ExUnit.Case, async: true
+
+  defmodule App do
+    use Dynamo
+
+    def service(conn) do
+      conn.resp(200, "Ok")
+    end
+
+    config :dynamo,
+      otp_app: :dynamo,
+      static_root: "test/fixtures/static",
+      static_route: "/static",
+      cache_static: true
+  end
+
+  defmodule NoCacheApp do
+    use Dynamo
+
+    def service(conn) do
+      conn.resp(200, "Ok")
+    end
+
+    config :dynamo,
+      otp_app: :dynamo,
+      static_root: "test/fixtures/static",
+      static_route: "/static",
+      cache_static: false
+  end
+
+  @asset "test/fixtures/static/file.txt"
+
+  def setup(_) do
+    File.touch!(@asset, { { 2010, 1, 1 }, { 0, 0, 0 } })
+    Dynamo.Static.start_link(App)
+  end
+
+  def teardown(_) do
+    Dynamo.Static.stop(App)
+  end
+
+  test "looks up a file" do
+    assert Dynamo.Static.lookup(App, "file.txt") ==  "/static/file.txt?63429523200"
+    assert Dynamo.Static.lookup(App, "/file.txt") == "/static/file.txt?63429523200"
+  end
+
+  test "uses cached version" do
+    assert Dynamo.Static.lookup(App, "file.txt") ==  "/static/file.txt?63429523200"
+    File.touch!(@asset, { { 2012, 1, 1 }, { 0, 0, 0 } })
+
+    assert Dynamo.Static.lookup(App, "file.txt") == "/static/file.txt?63429523200"
+    assert Dynamo.Static.lookup(App, "/file.txt") == "/static/file.txt?63429523200"
+  end
+
+  test "does not timestamp directories" do
+    assert Dynamo.Static.lookup(App, "/") ==  "/static"
+  end
+
+  test "does not cache when configured as so" do
+    Dynamo.Static.start_link(NoCacheApp)
+
+    assert Dynamo.Static.lookup(NoCacheApp, "file.txt") ==  "/static/file.txt?63429523200"
+    File.touch!(@asset, { { 2012, 1, 1 }, { 0, 0, 0 } })
+    assert Dynamo.Static.lookup(NoCacheApp, "file.txt") == "/static/file.txt?63492595200"
+  after
+    Dynamo.Static.stop(NoCacheApp)
+  end
+end
