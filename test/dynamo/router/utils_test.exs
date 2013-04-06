8cb73718062e5f21849c3e3622b905742b6a9c75
José Valim <jose.valim@plataformatec.com.br>
2013-04-06 14:30:51 -0600

    Fix utils tests

--- test/dynamo/router/utils_test.exs
+++ test/dynamo/router/utils_test.exs
@@ -5,12 +5,6 @@ defmodule Dynamo.Router.UtilsTest do
 
   use ExUnit.Case, async: true
 
-  defmacrop assert_quoted(left, right) do
-    quote do
-      assert quote(var_context: nil, do: unquote(left)) == unquote(right)
-    end
-  end
-
   test "root" do
     assert R.split("/") == []
     assert R.split("") == []
@@ -32,36 +26,36 @@ defmodule Dynamo.Router.UtilsTest do
   end
 
   test "generate match with literal" do
-    assert_quoted { [], ["foo"] }, R.generate_match("/foo")
-    assert_quoted { [], ["foo"] }, R.generate_match("foo")
+    assert quote(hygiene: [vars: false], do: { [], ["foo"] }) == R.generate_match("/foo")
+    assert quote(hygiene: [vars: false], do: { [], ["foo"] }) == R.generate_match("foo")
   end
 
   test "generate match with identifier" do
-    assert_quoted { [:id], ["foo", id] }, R.generate_match("/foo/:id")
-    assert_quoted { [:username], ["foo", username] }, R.generate_match("foo/:username")
+    assert quote(hygiene: [vars: false], do: { [:id], ["foo", id] }) == R.generate_match("/foo/:id")
+    assert quote(hygiene: [vars: false], do: { [:username], ["foo", username] }) == R.generate_match("foo/:username")
   end
 
   test "generate match with literal plus identifier" do
-    assert_quoted { [:id], ["foo", "bar-" <> id] }, R.generate_match("/foo/bar-:id")
-    assert_quoted { [:username], ["foo", "bar" <> username] }, R.generate_match("foo/bar:username")
+    assert quote(hygiene: [vars: false], do: { [:id], ["foo", "bar-" <> id] }) == R.generate_match("/foo/bar-:id")
+    assert quote(hygiene: [vars: false], do: { [:username], ["foo", "bar" <> username] }) == R.generate_match("foo/bar:username")
   end
 
   test "generate match only with glob" do
-    assert_quoted { [:bar], bar }, R.generate_match("*bar")
-    assert_quoted { [:glob], glob }, R.generate_match("/*glob")
+    assert quote(hygiene: [vars: false], do: { [:bar], bar }) == R.generate_match("*bar")
+    assert quote(hygiene: [vars: false], do: { [:glob], glob }) == R.generate_match("/*glob")
 
-    assert_quoted { [:bar], ["id-" <> _ | _] = bar }, R.generate_match("id-*bar")
-    assert_quoted { [:glob], ["id-" <> _ | _] = glob }, R.generate_match("/id-*glob")
+    assert quote(hygiene: [vars: false], do: { [:bar], ["id-" <> _ | _] = bar }) == R.generate_match("id-*bar")
+    assert quote(hygiene: [vars: false], do: { [:glob], ["id-" <> _ | _] = glob }) == R.generate_match("/id-*glob")
   end
 
   test "generate match with glob" do
-    assert_quoted { [:bar], ["foo" | bar] }, R.generate_match("/foo/*bar")
-    assert_quoted { [:glob], ["foo" | glob] }, R.generate_match("foo/*glob")
+    assert quote(hygiene: [vars: false], do: { [:bar], ["foo" | bar] }) == R.generate_match("/foo/*bar")
+    assert quote(hygiene: [vars: false], do: { [:glob], ["foo" | glob] }) == R.generate_match("foo/*glob")
   end
 
   test "generate match with literal plus glob" do
-    assert_quoted { [:bar], ["foo" | ["id-" <> _ | _] = bar] }, R.generate_match("/foo/id-*bar")
-    assert_quoted { [:glob], ["foo" | ["id-" <> _ | _] = glob] }, R.generate_match("foo/id-*glob")
+    assert quote(hygiene: [vars: false], do: { [:bar], ["foo" | ["id-" <> _ | _] = bar] }) == R.generate_match("/foo/id-*bar")
+    assert quote(hygiene: [vars: false], do: { [:glob], ["foo" | ["id-" <> _ | _] = glob] }) == R.generate_match("foo/id-*glob")
   end
 
   test "generate invalid match with segments after glob" do
