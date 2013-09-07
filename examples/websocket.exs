494e7bea9353fb603439de795d249e307a30f989
Mathieu Lajugie <mathieul@gmail.com>
2013-09-07 05:31:52 -0700

    simplify websocket example and add client example template

--- examples/websocket.exs
+++ examples/websocket.exs
@@ -2,40 +2,25 @@
 #
 #     mix run -r examples/websocket.exs --no-halt
 #
-# Cowboy Websocket Handler resources:
-#
-#   * User guide: http://ninenines.eu/docs/en/cowboy/HEAD/guide/ws_handlers
-#   * Reference:  http://ninenines.eu/docs/en/cowboy/HEAD/manual/cowboy_websocket_handler
+# Open a browser supporting websockets at URL http://localhost:3030 and wait for 3 seconds.
 #
 defmodule Websocket do
-  defmodule Teller do
-    def start, do: :global.register_name(:teller, spawn(__MODULE__, :listen, []))
-    def server, do: :global.whereis_name(:teller)
-    def register(pid), do: server <- { :register, pid }
-    def tell(message), do: server <- { :tell, message }
-    def listen(registrees // []) do
-      receive do
-        { :register, pid } -> listen([ pid | registrees ])
-        { :tell, message } ->
-          Enum.each registrees, fn pid ->
-            pid <- { :message, message }
-          end
-          listen(registrees)
-      end
-    end
-  end
-
-  defmodule WebsocketHandler do
+  # Cowboy Websocket Handler resources:
+  #
+  #   * User guide: http://ninenines.eu/docs/en/cowboy/HEAD/guide/ws_handlers
+  #   * Reference:  http://ninenines.eu/docs/en/cowboy/HEAD/manual/cowboy_websocket_handler
+  #
+  defmodule Handler do
     @behaviour :cowboy_websocket_handler
     def init({:tcp, :http}, _req, _opts), do: { :upgrade, :protocol, :cowboy_websocket }
     def websocket_init(_transport_name, req, _opts) do
-      Teller.register(self)
+      :timer.send_interval(3000, { :message, "Are we there yet?" })
       { :ok, req, :no_state }
     end
-    def websocket_handle( { :text, message }, req, state) do
-      { :reply, {:text, message }, req, state }
-    end
     def websocket_info({ :message, message }, req, state) do
+      { :reply, { :text, message }, req, state }
+    end
+    def websocket_handle({ :text, message }, req, state) do
       { :reply, { :text, message }, req, state }
     end
     def websocket_terminate(_reason, _req, _state), do: :ok
@@ -45,22 +30,19 @@ defmodule Websocket do
     use Dynamo
     use Dynamo.Router
 
-    config :dynamo, compile_on_demand: false
+    config :dynamo, templates_paths: [ Path.expand("../websocket", __FILE__) ]
     config :server,
       port: 3030,
       dispatch: [{ :_, [
-        {"/websocket", WebsocketHandler, [] },
+        {"/websocket", Handler, [] },
         {:_, Dynamo.Cowboy.Handler, __MODULE__ }
       ] }]
 
-    get "/*segments" do
-      message = Enum.join(conn.route_params[:segments], " ")
-      Teller.tell(message)
-      conn.resp_body("message sent: #{message}")
+    get "/" do
+      render conn, "index.html"
     end
   end
 end
 
-Websocket.Teller.start
 Websocket.Router.start_link
 Websocket.Router.run
