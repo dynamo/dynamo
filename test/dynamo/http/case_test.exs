0a09d7111c2007bbb84a99c32246227ffdb4aa4b
Mathieu Lajugie <mathieu@caring.com>
2014-02-06 23:58:13 -0800

    update HTTP case method macros to allow passing an existing connection and a body

--- test/dynamo/http/case_test.exs
+++ test/dynamo/http/case_test.exs
@@ -20,6 +20,13 @@ defmodule Dynamo.HTTP.CaseTest do
       conn = conn.fetch :body
       conn.send 200, conn.req_body
     end
+
+    post "/post_cookie" do
+      conn = conn.fetch :cookies
+      conn = conn.fetch :body
+      { "token", token, _ } = List.keyfind(conn.resp_cookies, "token", 0)
+      conn.send 200, "body(#{conn.req_body}) token(#{token})"
+    end
   end
 
   use ExUnit.Case
@@ -51,5 +58,11 @@ defmodule Dynamo.HTTP.CaseTest do
   test "sees the request body from a post with HashDict" do
     conn = post("/test_post", [{"foo", "bar"}, {"bar", "foo"}])
     assert conn.sent_body == "foo=bar&bar=foo"
+  end
+
+  test "sees the cookie from the post request" do
+    conn = put_cookie conn(:post, "/"), :token, "1a2b3c"
+    conn = post(conn, "/post_cookie", "hello")
+    assert conn.sent_body == "body(hello) token(1a2b3c)"
   end
 end
