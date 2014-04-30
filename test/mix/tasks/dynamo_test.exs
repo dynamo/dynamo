32710f6e55fd05d7e19762069071dbf567afbd7d
parroty <parroty00@gmail.com>
2014-04-30 22:24:53 +0900

    Update elixir-lang/dynamo with dynamo/dynamo in Mix.Tasks.DynamoTest

--- test/mix/tasks/dynamo_test.exs
+++ test/mix/tasks/dynamo_test.exs
@@ -24,7 +24,7 @@ defmodule Mix.Tasks.DynamoTest do
         assert file =~ ~r(app: :my_app)
         assert file =~ ~r(version: "0.0.1")
         assert file =~ ~r({ :dynamo)
-        assert file =~ ~r(github: "elixir-lang/dynamo")
+        assert file =~ ~r(github: "dynamo/dynamo")
       end
 
       assert_file "README.md", ~r(# MyApp)
